989b854285c1ff18008b633d38b7ede3d43d0bae
mahdighadiriii <mahdighadiriafzal@gmail.com>
2023-06-11 22:02:32 +0330

    Update admin.py
    Create Class Custom User Admin
    Create Custom User Model

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 # Register your models here.
+
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ('email', 'is_superuser', 'is_active')
+    list_filter = ('email', 'is_superuser', 'is_active')
+    searching_fields = ('email',)
+    ordering = ('email',)
+    fieldsets = [
+        (
+            'Authentication',
+            {
+                "fields": ["email", "password",],
+            },
+        ),
+        (
+            "permissions",
+            {
+                "fields": ["is_staff", "is_active", "is_superuser"],
+            },
+        ),
+    ]
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active'),}),)
+    
+admin.site.register(User, CustomUserAdmin)
