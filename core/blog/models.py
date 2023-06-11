05e37d34ff6734e0d728117a19e17f2e4c4c23d6
mahdighadiriii <mahdighadiriafzal@gmail.com>
2023-06-11 12:51:19 +0430

    Update models.py (blog)
    created class post and category

--- core/blog/models.py
+++ core/blog/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+'''
+this is a class to define posts for blog app
+'''
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(null=True, blank=True)
+    title = models.CharField(max_length=250)
+    content = models.TextField()
+    status = models.BooleanField()
+    category = models.ForeignKey('category', on_delete=models.SET_NULL, null=True)
+
+    created_date = models.DateField(auto_now_add=True)
+    updated_date = models.DateField(auto_now=True)
+    published_date = models.DateField()
+
+    def __str__(self):
+        return self.title
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
+    
