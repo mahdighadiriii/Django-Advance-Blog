de137f88fb9f544612f12035bfcbf1e2762b5fbc
mahdighadiriii <mahdighadiriafzal@gmail.com>
2023-06-18 15:00:56 +0330

    Update views.py

--- core/blog/views.py
+++ core/blog/views.py
@@ -2,16 +2,25 @@ from django.shortcuts import render
 from django.views.generic.base import TemplateView
 from .models import Post
 # Create your views here.
+
+
+
+# Function Base View Show a Template
+'''
 def indexView(request):
     name = "ali"
     context = {"name":name}
     return render(request,"index.html",context)
-
+'''
 
 class IndexView(TemplateView):
     template_name = 'index.html'
-
+    '''
+    a class based view to show index page 
+    '''
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["name"] = "ali"
         return context
+
+
