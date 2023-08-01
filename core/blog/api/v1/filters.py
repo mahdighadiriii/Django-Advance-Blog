91ead696e150d9374ff0d5bc9fe5c9065b968b0c
mahdighadiriii <mahdighadiriafzal@gmail.com>
2023-08-01 20:18:46 +0330

    add more filters and paginator
    added more filter and create filters.py in api folder and create new class for more filters. added paginator and create paginations.py in api folder

---
+++ core/blog/api/v1/filters.py
@@ -0,0 +1,14 @@
+from rest_framework import generics
+from django_filters import rest_framework as filters
+
+
+
+class ProductFilter(filters.FilterSet):
+    min_price = filters.NumberFilter(field_name="price", lookup_expr='gte')
+    max_price = filters.NumberFilter(field_name="price", lookup_expr='lte')
+
+    class Meta:
+        model = Product
+        fields = ['category', 'in_stock']
+
+
--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -11,6 +11,8 @@ from rest_framework import viewsets
 from rest_framework.generics import GenericAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .permissions import IsOwnerOrReadOnly
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter,OrderingFilter
+from .paginations import DefaultPagination
 
 
 """@api_view()
@@ -109,8 +111,14 @@ class PostModelViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
     serializer_class = PostSerializer
     queryset = Post.objects.filter(status=True)
-    filter_backends = [DjangoFilterBackend]
+    filter_backends = [DjangoFilterBackend,SearchFilter,OrderingFilter]
     filterset_fields = ['category', 'author', 'status']
+    search_fields = ['title', 'content']
+    ordering_fields = ['published_date']
+    pagination_class = DefaultPagination
+
+
+
 
 class CategoryModelViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticatedOrReadOnly]
