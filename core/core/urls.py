56257ddf6e99ba2e98cb268e3f3812c78e1398c8
mahdighadiriii <mahdighadiriafzal@gmail.com>
2023-08-03 11:56:05 +0330

    api for accounts
    created api for app accounts and set config
    registration with serializer and created urls

--- core/core/urls.py
+++ core/core/urls.py
@@ -43,7 +43,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls')),
     path('blog/', include('blog.urls')),
     path('swagger/output.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
