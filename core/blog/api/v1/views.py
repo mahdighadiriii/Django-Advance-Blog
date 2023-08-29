a63d811104940c7ccb41acba31990ad3cdd6e85d
mahdighadiriii <mahdighadiriafzal@gmail.com>
2023-08-29 22:22:42 +0330

    ci
    reformatted

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -1,4 +1,3 @@
-from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from .serializers import PostSerializer, CategorySerializer
 from ...models import Post, Category
@@ -21,99 +20,6 @@ from .permissions import IsOwnerOrReadOnly
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
 from .paginations import DefaultPagination
-
-
-"""@api_view()
-def postList(request):
-    posts = Post.objects.filter(status=True)
-    serializer = PostSerializer(posts, many=True)
-    return Response(serializer.data)
-
-@api_view()
-def postDetail(request,id):
-    return Response(id)"""
-
-
-'''
-@api_view(["GET","POST"])
-def PostList(request):
-    if request.method == "GET":
-        posts = Post.objects.filter(status=True)
-        serializer = PostSerializer(posts,many=True)
-        return Response(serializer.data)
-    elif request.method == "POST":
-        serializer = PostSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-
-
-@api_view()
-def PostDetail(request,id):
-    post = get_object_or_404(Post,pk=id,status=True)
-    serializer = PostSerializer(post)
-    return Response(serializer.data)
-
-
-
-class PostList(APIView):
-    """getting a list of posts and creating new posts"""
-    permission_classes = [IsAuthenticatedOrReadOnly]
-    serializer_class = PostSerializer
-
-    def get(self,request):
-        """retrieving a list of posts"""
-        posts = Post.objects.filter(status=True)
-        serializer = PostSerializer(posts,many=True)
-        return Response(serializer.data)
-    
-    def post(self,request):
-        """creating a post with provided data"""
-        serializer = PostSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-    
-
-
-
-
-class PostDetail(APIView):
-    """getting detail of the post and edit plus removing it"""
-
-    permission_classes = [IsAuthenticatedOrReadOnly]
-    serializer_class = PostSerializer
-
-    def get(self,request,id):
-        """retrieving the post data"""
-        post = get_object_or_404(Post,pk=id,status=True)
-        serializer = self.serializer_class(post)
-        return Response(serializer.data)
-    
-    def put(self,request,id):
-        """editing the post data"""
-
-        post = get_object_or_404(Post,pk=id,status=True)
-        serializer = PostSerializer(post,data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data) 
-
-    def delete(self,request,id):
-        post = get_object_or_404(Post,pk=id,status=True)
-        post.delete()
-        return Response({"detail":"item removed successfully"},status=status.HTTP_204_NO_CONTENT)
-
-class PostList(ListCreateAPIView):
-    permission_classes = [IsAuthenticatedOrReadOnly]
-    serializer_class = PostSerializer
-    queryset = Post.objects.filter(status=True)
-
-class PostDetail(RetrieveUpdateDestroyAPIView):
-    """getting detail of post and edit plus removing it"""
-    permission_classes = [IsAuthenticatedOrReadOnly]
-    serializer_class = PostSerializer
-    queryset = Post.objects.filter(status=True)'''
 
 
 class PostModelViewSet(viewsets.ModelViewSet):
--- core/blog/tests/test_post_api.py
+++ core/blog/tests/test_post_api.py
@@ -37,19 +37,6 @@ class TestPostApi:
         response = api_client.post(url, data)
         assert response.status_code == 401
 
-   # def test_create_post_response_201_status(self, api_client, common_user):
-   #     url = reverse("blog:api-v1:post-list")
-   #     data = {
-   #         "title": "test",
-   #         "content": "description",
-   #         "status": True,
-   #         "published_date": datetime.now(),
-   #     }
-   #     user = common_user
-   #     api_client.force_authenticate(user=user)
-   #     response = api_client.post(url, data)
-   #     assert response.status_code == 201
-
     def test_create_post_invalid_data_response_400_status(
         self, api_client, common_user
     ):
