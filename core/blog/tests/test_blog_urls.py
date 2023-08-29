f4e575f664ed9295bb10043b577999315aa428de
mahdighadiriii <mahdighadiriafzal@gmail.com>
2023-08-29 18:34:06 +0330

    Test
    test form with TestCase

--- core/blog/tests/test_blog_urls.py
+++ core/blog/tests/test_blog_urls.py
@@ -1,11 +1,20 @@
-from django.test import TestCase
-from django.urls import reverse, resolve
-from ..views import IndexView,PostCreateView
+from django.test import TestCase,SimpleTestCase
+from django.urls import reverse,resolve
+from ..views import IndexView,PostListView,PostDetailView
+# Create your tests here.
 
+class TestUrl(SimpleTestCase):
 
-class TestUrl(TestCase):
-    
-        
-    def test_blog_create_url_resolve(self):
-        url = reverse('blog:post-create')
-        self.assertEquals(resolve(url).func.view_class,PostCreateView)
+    def test_blog_index_url_resolve(self):
+        url = reverse('blog:index')
+        self.assertEquals(resolve(url).func.view_class,IndexView)
+
+    def test_blog_post_list_url_resolve(self):
+        url = reverse('blog:post-list')
+        self.assertEquals(resolve(url).func.view_class,PostListView)
+
+    def test_blog_post_detail_url_resolve(self):
+        url = reverse('blog:post-detail',kwargs={'pk':1})
+        self.assertEquals(resolve(url).func.view_class,PostDetailView)
+
+ 
